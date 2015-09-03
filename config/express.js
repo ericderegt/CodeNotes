11895a1bafaba67afd51c75d5de8ce0499c97578
Eric deRegt <ericderegt@gmail.com>
2015-09-03 18:29:42 -0400

    added basic index for views

--- config/express.js
+++ config/express.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
 
+
+// need to add routes
+// var indexRoutes = require('../app/routes/index');
 
 module.exports = function() {
 
   var app = express();
 
+  // set up views
+  app.set('views', path.join(__dirname, '../app/views'));
+  app.set('view engine', 'jade');
+
+  app.use(logger('dev'));
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+
   app.get('/', function(req,res) {
-    res.send('Getting this app started');
+    res.render('index');
   });
 
   // return Express server instance, called in server.js
