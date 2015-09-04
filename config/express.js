abf91920c6f4152e7155800fe91a60079cb1c8d5
Eric deRegt <ericderegt@gmail.com>
2015-09-04 17:30:51 -0400

    add some middleware and started routes

--- config/express.js
+++ config/express.js
@@ -1,27 +1,35 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 
-// need to add routes
-// var indexRoutes = require('../app/routes/index');
+// Routes
+var indexRoutes = require('../app/routes/index');
 
 module.exports = function() {
 
   var app = express();
 
-  // set up views
+  // set view path and engine
   app.set('views', path.join(__dirname, '../app/views'));
   app.set('view engine', 'jade');
 
-  app.use(logger('dev'));
+  if (process.env.NODE_ENV === 'development') {
+    // if development, use logging and disable view cache
+    app.use(logger('dev'));
+    app.set('view cache', false);
+  }
 
+  // set parsers & static folder
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
 
 
-  app.get('/', function(req,res) {
-    res.render('index');
-  });
+  app.use('/', indexRoutes);
 
   // return Express server instance, called in server.js
   return app;
